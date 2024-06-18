091baf7be5221f35d35205a14449f4c9c2256b62
BlackZ <duchinh0306@gmail.com>
2024-06-19 01:43:31 +0700

    commit

--- ZStore DAL/Interface/IProductRepository.cs
+++ ZStore DAL/Interface/IProductRepository.cs
@@ -8,9 +8,17 @@ using ZStore_BLL.Models;
 namespace ZStore_DAL.Interface
 {
     public interface IProductRepository
-    {
+    {   
+
+
+        //lấy ra toàn bộ danh sách sản phẩm
         Task<IEnumerable<Product>> GetProductsAsync();
+
+        //lấy ra 1 sản phẩm theo id
         Task<Product> GetProductByIdAsync(int ID);
+
+        //lấy ra một sản phẩm theo category id
+
         Task<bool> AddProductAsync(Product product);
         Task<bool> UpdateProductAsync(Product product);
         Task<bool> DeleteProductAsycn(int ID);
